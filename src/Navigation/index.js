1cdc42a68115e73ad8d9f5713fc70fca44e3757e
hburnton <hburnton@gmail.com>
2022-12-31 11:36:43 -0700

    add header and nav

---
+++ src/Navigation/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function Navigation() {
+    console.log('test');
+    return (
+        <nav>
+                <ul>
+                    <li>
+                        <a href="#about">About Me</a>
+                    </li>
+                    <li>
+                        <a href="#projects">Work</a>
+                    </li>
+                    <li>
+                        <a href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </nav>
+    );
+}
+  
